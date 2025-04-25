b945ca6e71b1ca6c72347ed739017ebcd78addba
Максим Кучишкин <kuchishkin.m@gmail.com>
2025-04-25 12:09:26 +0300

    update

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { ReactComponent as LogoIcon } from '../../images/logo.svg';
+import { ReactComponent as MenuIcon } from '../../images/icon-menu.svg';
 import { NavItem } from '../nav-item';
 import { NavMenu } from '../nav-menu';
 import { COMPANY, FEATURES } from './constants';
@@ -9,7 +10,7 @@ export const Header = () => {
   return (
     <header className='flex items-center'>
       <LogoIcon />
-      <nav className='flex space-x-6 ml-8 items-center'>
+      <nav className= 'hidden xl:flex space-x-6 ml-8 items-center'>
         <NavItem text='Feature'>
           <NavMenu items={ FEATURES }/>
         </NavItem>
@@ -19,9 +20,12 @@ export const Header = () => {
         <NavItem text='Careers'/>
         <NavItem text='About'/>
       </nav>
-      <div className="ml-auto flex space-x-5">
+      <div className="hidden ml-auto xl:flex space-x-5">
         <Button>Loggin</Button>
         <Button hasBorder={ true }>Register</Button>
+      </div>
+      <div className="flex xl:hidden ml-auto cursor-pointer z-30">
+        <MenuIcon/>
       </div>
     </header>
   )
--- src/components/main-section/main-section.jsx
+++ src/components/main-section/main-section.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '../button'
 import MeinImage from '../../images/image-hero-desktop.png'
+import MeinImageMobile from '../../images/image-hero-mobile.png'
 import { ReactComponent as Databiz } from '../../images/client-databiz.svg'
 import { ReactComponent as Audiophile } from '../../images/client-audiophile.svg'
 import { ReactComponent as Meet } from '../../images/client-meet.svg'
@@ -8,12 +9,24 @@ import { ReactComponent as Maker } from '../../images/client-maker.svg'
 
 export const MainSection = () => {
   return (
-    <section className='w-full flex mt-6'>
-      <div className='relative w-2/4 text-left mt-12'>
-        <h1 className='text-8xl font-black mt-6 whitespace-pre-line'>{`Make\n remote work`}</h1>
-        <p className="text-medium-gray text-lg mt-12 whitespace-pre-line">{`Lorem, ipsum dolor sit amet consectetur adipisicing elit.\n Facilis ad nisi laboriosam. Nihil, minima dicta cum, numquam similique doloremque officia quo asperiores sequi libero maiores\n nobis molestias placeat dolorum itaque.`}</p>
+    <section className='w-full flex-col xl:flex-row flex mt-20 h-screen justify-between'>
+      <div className='order-2 xl:w-2/4 xl:order-1 text-center xl:text-left relative text-left mt-12 xl:mt-60'>
+        <h1 className='text-3xl xl:text-8xl font-black xl:whitespace-pre-line'>{`Make\n remote work`}</h1>
+        <p className="text-medium-gray text-lg my-12 whitespace-pre-line">{`Lorem, ipsum dolor sit amet consectetur adipisicing elit.\n Facilis ad nisi laboriosam. Nihil, minima dicta cum, numquam similique doloremque officia quo asperiores sequi libero maiores\n nobis molestias placeat dolorum itaque.`}</p>
+        <Button isFilled={ true }>Learn more</Button>
+        <div className="flex justify-around xl:absolute bottom-2 w-full mt-20">
+          <Databiz/>
+          <Audiophile/>
+          <Meet/>
+          <Maker/>
+        </div>
       </div>
-      <Button isFilled={ true }></Button>
+      <div className="hidden xl:flex xl:w-2/4 xl:order-2">
+        <img src={ MeinImage } alt="man"/>
+      </div>
+      <div className="flex xl:hidden w-full justify-center order-1">
+        <img src={ MeinImageMobile } alt="man"/>
+      </div>
     </section>
   )
 }
