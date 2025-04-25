4494d86cb6a2842f6fd6e2c98193aa86020c3fb3
Максим Кучишкин <kuchishkin.m@gmail.com>
2025-04-25 12:34:53 +0300

    fix

--- src/components/main-section/main-section.jsx
+++ src/components/main-section/main-section.jsx
@@ -10,7 +10,7 @@ import { ReactComponent as Maker } from '../../images/client-maker.svg'
 export const MainSection = () => {
   return (
     <section className='w-full flex-col xl:flex-row flex mt-20 h-screen justify-between'>
-      <div className='order-2 xl:w-2/4 xl:order-1 text-center xl:text-left relative text-left mt-12 xl:mt-60'>
+      <div className='relative order-2 xl:w-2/4 xl:order-1 text-center xl:text-left text-left mt-12 xl:mt-60'>
         <h1 className='text-3xl xl:text-8xl font-black xl:whitespace-pre-line'>{`Make\n remote work`}</h1>
         <p className="text-medium-gray text-lg my-12 whitespace-pre-line">{`Lorem, ipsum dolor sit amet consectetur adipisicing elit.\n Facilis ad nisi laboriosam. Nihil, minima dicta cum, numquam similique doloremque officia quo asperiores sequi libero maiores\n nobis molestias placeat dolorum itaque.`}</p>
         <Button isFilled={ true }>Learn more</Button>
